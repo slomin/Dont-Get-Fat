c7f78194f5be54dfba6a840a6f58d46a9b78c972
jan <jan@kotlinblog.com>
2017-09-13 20:27:04 +0200

    Adding fragment switching in MainActivity

--- app/src/main/java/com/kotlinblog/dontgetfat/main/MainActivity.kt
+++ app/src/main/java/com/kotlinblog/dontgetfat/main/MainActivity.kt
@@ -4,22 +4,31 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.kotlinblog.dontgetfat.R
+import com.kotlinblog.dontgetfat.extensions.addFragment
+import com.kotlinblog.dontgetfat.extensions.replaceFragment
+import com.kotlinblog.dontgetfat.main.left.LeftFragment
+import com.kotlinblog.dontgetfat.main.middle.MiddleFragment
+import com.kotlinblog.dontgetfat.main.right.RightFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val mLeftFragment = LeftFragment()
+    private val mMiddleFragment = MiddleFragment()
+    private val mRightFragment = RightFragment()
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_left_fragment -> {
-                message.setText(R.string.title_left_fragment)
+                replaceFragment(mLeftFragment, R.id.fragmentContainer)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_middle_fragment -> {
-                message.setText(R.string.title_middle_fragment)
+                replaceFragment(mMiddleFragment, R.id.fragmentContainer)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_right_fragment -> {
-                message.setText(R.string.title_right_fragment)
+                replaceFragment(mRightFragment, R.id.fragmentContainer)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -29,7 +38,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment(mLeftFragment, R.id.fragmentContainer)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
 
 }
